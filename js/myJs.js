0fbe5df81a73e281769eac9dc45a87b321fde63a
Vanh1803 <95005775+vanhmaibenem@users.noreply.github.com>
2024-09-07 21:56:01 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
     Swal.fire({
       title: textConfig.text1,
       text: textConfig.text2,
-      imageUrl: "img/cuteCat.jpg",
+      imageUrl: "img/loopy.jpg",
       imageWidth: 300,
       imageHeight: 300,
       background: '#fff url("img/iput-bg.jpg")',
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "http://fb.com";
+            window.location = "https://www.facebook.com/profile.php?id=100000393560870";
           },
         });
       }
