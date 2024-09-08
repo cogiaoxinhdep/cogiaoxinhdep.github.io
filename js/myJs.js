e05cf2b2e421aeb947d27d2f35c4bbf5401cf831
Vanh1803 <95005775+vanhmaibenem@users.noreply.github.com>
2024-09-08 09:20:04 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://www.facebook.com/profile.php?id=100000393560870";
+            window.location = "https://vanhiuemnhat.github.io/";
           },
         });
       }
